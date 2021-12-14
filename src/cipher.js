5d5d10d9ca1cb856af68fcc7223df81f103ecd62
Isis <isisdanielaaguilar@gmail.com>
2021-12-14 18:04:53 -0300

    Actualización de código fuente

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,48 @@
 const cipher = {
-  // ...
-};
+
+ encode: (userInput, offset) => {
+ 
+  let stringSplit = userInput.split('');
+  let stringOutput = ''; 
+
+  stringSplit.forEach( (character)=> {
+    if( character == ' ' || character == '.' || character ==  ',' )
+      stringOutput = stringOutput + character
+    else{
+    let codeInput = character.charCodeAt(); 
+    let codeOutput = Math.abs((codeInput - 65 + offset) %26 + 65);
+    stringOutput = stringOutput.toUpperCase() + String.fromCharCode(codeOutput);
+  }});
+ return stringOutput;
+},
+ decode: (userInput,offset) => {
+          
+ let stringSplit = userInput.split('');
+ let stringOutput = ''; 
+ 
+ stringSplit.forEach( (character)=> {
+  if( character == ' ' || character == '.' || character ==  ',' )
+    stringOutput = stringOutput + character
+  else{
+  let codeInput = character.charCodeAt(); 
+
+  // let codeOutput = (codeInput - 65 + offset) %26 + 65;
+  // let returnPosition = Math.abs(offset - codeInput);
+  
+  // let codeOutput = Math.abs((offset - 65 + codeInput) %26 + 65) 
+  let codeOutput = Math.abs(offset - codeInput)
+  
+  stringOutput = stringOutput.toUpperCase() + String.fromCharCode(codeOutput);
+}
+   
+  console.log('decode input ' + userInput)
+  console.log('decode offset ' + offset)
+  console.log('decode output ' + stringOutput)
+ 
+})
+ return stringOutput;
+},
+
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let userName = document.getElementById('userName');
+let userNameView = document.getElementById('userNameView');
+
+
+document.getElementById('cipher').addEventListener("click", function() {
+  let offset = parseInt(document.getElementById('range').value);  // input del usuario
+  let userInput= document.getElementById('input-user').value.toUpperCase();  // rango del usuario
+  document.getElementById('parrafo').innerHTML = cipher.encode(userInput, offset);
+  });
+
+  document.getElementById('decipher').addEventListener("click", function() {
+    let offset = parseInt(document.getElementById('range').value);  // input del usuario
+    let userInput= document.getElementById('input-user').value.toUpperCase();  // rango del usuario
+    document.getElementById('parrafo').innerHTML = cipher.decode(userInput, offset);
+    });
+
+  // Funcion de Index.html
+  function username() {
+    userName.addEventListener("keyup", function(){
+    userNameView.innerHTML = document.getElementById('userName').value;
+  });
+  } username()
+  
